79deab171a03932a6c072808dcbaee3e7d62407a
Sayed Abdel Badie <34669926+psydology@users.noreply.github.com>
2021-06-16 02:03:50 +0200

    Update shelf.js

--- src/components/shelf.js
+++ src/components/shelf.js
@@ -20,7 +20,7 @@ class Shelf extends Component {
                         <li key = {book.id}>
                         <div className="book">
                           <div className="book-top">
-                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks ? book.imageLinks.thumbnail : '../icons/book-reader.svg'})` }}></div>
                             <div className="book-shelf-changer">
                               <select value = {book.shelf} onChange= {(event) => ChangeBShelf (book , event.target.value)}>
                                 <option value="move" disabled>Move to...</option>
